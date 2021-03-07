240e5f26ec00b34f693f1790afc7d3752b8159c4
2019211001000911-ZhangZhenBin <1>
2021-03-07 22:47:35 +0800

    First Commit

---
+++ src/main/java/com/example/ZhangZhenbin2019211001000911/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.ZhangZhenbin2019211001000911;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
