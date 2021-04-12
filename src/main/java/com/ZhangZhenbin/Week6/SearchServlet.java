2eb83cd07ef46a260375701cf8780ffaf3f0295b
2019211001000911-ZhangZhenBin <1>
2021-04-12 16:13:05 +0800

    Week6 commit

--- src/main/java/com/ZhangZhenbin/Week6/SearchServlet.java
+++ src/main/java/com/ZhangZhenbin/Week6/SearchServlet.java
@@ -13,18 +13,20 @@ public class SearchServlet extends HttpServlet {
 
         String Text = request.getParameter("text");
         String Select = request.getParameter("search");
-        response.setContentType("text/html");
-        if(Text == null){
+        //response.setContentType("text/html");
+        if (Text.equals("")) {
             response.sendRedirect("index.jsp");
-        }else if(Select.equals("baidu") ){
-            response.sendRedirect("https://www.baidu.com/s?wd="+Text);
-        }else if(Select.equals("bing")){
-            response.sendRedirect("https://cn.bing.com/search?q="+Text);
-        }else if(Select.equals("google")){
-            response.sendRedirect("https://www.google.com/search?q="+Text);
-        }
+        } else {
+            if (Select.equals("baidu")) {
+                response.sendRedirect("https://www.baidu.com/s?wd=" + Text);
+            } else if (Select.equals("bing")) {
+                response.sendRedirect("https://cn.bing.com/search?q=" + Text);
+            } else if (Select.equals("google")) {
+                response.sendRedirect("https://www.google.com/search?q=" + Text);
+            }
 
-}
+        }
+    }
 
 
 
